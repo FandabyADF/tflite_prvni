898a1bfec7be776b9eafd7134b219245a6acc7a1
Frantisek Steinhauser <frantisek.steinhauser@byadf.cz>
2022-03-28 14:47:54 +0200

    stringy(floaty) v datasetu jsou přepsané do formátu int

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from absl import logging
 
 logging.set_verbosity(logging.ERROR)
 
-spec = model_spec.get('efficientdet_lite4')
+spec = model_spec.get('efficientdet_lite3')
 # windows celkovy dataset
 #imgs = r'C:\Users\f.steinhauser\datasety\vsechny_rcnn\img2'
 #annot = r'C:\Users\f.steinhauser\datasety\vsechny_rcnn\annot'
@@ -25,7 +25,7 @@ imgs = "img2"
 annot = "annot"
 
 #train_data, validation_data, test_data = object_detector.DataLoader.from_csv('gs://cloud-ml-data/img/openimage/csv/salads_ml_use.csv')
-train_data = object_detector.DataLoader.from_pascal_voc(imgs, annot, label_map={1: "apple", 2: "apples", 3: "damaged_apple"})
+train_data = object_detector.DataLoader.from_pascal_voc(imgs, annot, label_map={1: "apple"})
 
 #
 model = object_detector.create(train_data, model_spec=spec, batch_size=8, train_whole_model=True, validation_data=train_data)
@@ -36,6 +36,6 @@ print(model.evaluate(train_data))
 print("ohodnocena data tflitem: ")
 print(model.evaluate_tflite(train_data))
 
-model.export(export_dir='vytrenovana_maska_l4')
+model.export(export_dir='vytrenovana_maska_l3')
 
-model.evaluate_tflite('vytrenovana_maska_l4//model.tflite', train_data)
+model.evaluate_tflite('vytrenovana_maska_l3//model.tflite', train_data)
